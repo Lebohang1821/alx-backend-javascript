95fb0007be56c09a43fae9b0dd31faeb7eb0b67e
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-08 21:24:49 +0200

    Update 3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,4 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  // Calculate the sum of initial number and both expansions (default values provided).
   return initialNumber + expansion1989 + expansion2019;
 }
