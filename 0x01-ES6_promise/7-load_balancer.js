dab5cf2ef9fa248ddef5d61b82066108addc9936
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-09 18:44:16 +0200

    Update 7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
     .race([chinaDownload, USDownload])
-    .then((res) => res);
+    .then((res) => res); // Returns result of fastest download
 }
