48a524ee90bde6e7f331b47836cbd504b71825f3
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-09 18:07:54 +0200

    Task 5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-   // Concatenate array1, array2, and string into array
-  return [...array1, ...array2, ...string];
+  return [...array1, ...array2, ...string]; // Concatenate array1, array2, and string into array
 }
