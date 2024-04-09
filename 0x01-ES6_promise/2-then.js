c545e4a221ee2d0bcf792f39870ea1a4bc9ed5a1
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-09 18:40:05 +0200

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+    .finally(() => console.log('Got a response from the API')); // Log message after response
 }
