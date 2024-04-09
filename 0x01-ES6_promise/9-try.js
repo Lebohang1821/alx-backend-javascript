cb6cbb16eb5eeea93273d635d5f49178b57d58fd
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-09 18:45:50 +0200

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -9,5 +9,5 @@ export default function guardrail(mathFunction) {
     queue.push('Guardrail was processed');
   }
 
-  return queue;
+  return queue; // Return array containing results and messages
 }
