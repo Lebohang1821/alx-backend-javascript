96af122955871e6f9190572e2b3ca0abaa7894b2
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-10 12:03:19 +0200

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));  // It log error message
+    .catch(() => console.log('Signup system offline'));
 }
