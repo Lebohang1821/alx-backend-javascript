ba02af730e1ffbf50bb9f52d244e9c858136df3a
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-11 13:07:11 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,23 @@
-import Car from './10-car';
+import Car from './10-car'; // Importing Car class
 
+// Class for Electric Vehicle (EV) Car, extending Car
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this.range = range;
+    super(brand, motor, color); // Calling superclass constructor
+    this.range = range; // Setting the range of the EV Car
   }
 
   get range() {
-    return this._range;
+    return this._range; // Gets range of EV Car
   }
 
   set range(value) {
-    this._range = value;
+    this._range = value; // Sets range of the EV Car
   }
 
   cloneCar() {
     const Species = super.constructor[Symbol.species];
 
-    return new Species();
+    return new Species(); // Creates instance using species constructor
   }
 }
