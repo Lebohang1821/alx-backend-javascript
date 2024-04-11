030ebb82b943030733d7bff3129b6ecafa1d89b4
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-11 12:53:15 +0200

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,28 @@
 import Building from './5-building';
 
+// Defining class named SkyHighBuilding that extends Building class
 export default class SkyHighBuilding extends Building {
+  // Constructor method for creating instances of SkyHighBuilding
   constructor(sqft, floors) {
+    // Calling superclass (Building) constructor with square footage
     super(sqft);
+    // Assigning the number of floors of the sky-high building
     this.floors = floors;
   }
 
+  // Gets method for retrieving the number of floors
   get floors() {
     return this._floors;
   }
 
+  // Sets method for setting number of floors
   set floors(value) {
     this._floors = value;
   }
 
+  // Method to provide evacuation warning message for sky-high building
   evacuationWarningMessage() {
+    // Constructing and returning evacuation warning message
     return `Evacuate slowly the ${this.floors} floors`;
   }
 }
