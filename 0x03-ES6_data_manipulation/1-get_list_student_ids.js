8d28c0931d6aaea038cf0d625358af10bad99d93
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-15 20:12:40 +0200

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(students) {
   if (students instanceof Array) {
     return students.map((student) => student.id);
   }
-  return [];
+  return []; // Returns empty array if input is not array
 }
