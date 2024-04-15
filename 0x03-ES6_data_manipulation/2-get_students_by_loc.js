9751caa52f98b01a2ba96792965696dcd7a41ed0
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-15 20:14:33 +0200

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
     return students.filter((student) => student.location === city);
   }
-  return [];
+  return []; // Return empty array if input is not array.
 }
