bbc37be5e1d784a1dff31f0efe50ec79820642a5
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-15 20:26:42 +0200

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -5,6 +5,6 @@ export default function updateUniqueItems(map) {
   map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
-    }
+    } // It updates value to 100 if it's currently 1
   });
 }
