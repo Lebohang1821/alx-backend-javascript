304e864e99bc5492c20abe122c54e180dc7e1446
Lebohang1821 <millboylebohang@gmail.com>
2024-04-15 11:07:27 -0700

    task-0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,5 @@
 export default function getListStudents() {
   return [
-	  // Returns array of student objects with id
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
