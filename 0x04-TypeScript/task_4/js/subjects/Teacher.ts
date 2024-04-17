18e1ca771e64d279c334a9895524df3a76afa5c9
Lebohang1821 <millboylebohang@gmail.com>
2024-04-17 12:56:01 -0700

    task-4

---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,8 @@
+// It define namespace called Subjects
+namespace Subjects {
+  // Define interface for teachers
+  export interface Teacher {
+    firstName: string; // Property for first name of teacher
+    lastName: string;  // Property for last name of teacher
+  }
+}
