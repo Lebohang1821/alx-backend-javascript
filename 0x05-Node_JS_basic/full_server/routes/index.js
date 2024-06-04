98ce3aa13b62826b3b63883f02d90fe39861e9c3
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-04 17:40:52 +0200

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,15 +1,15 @@
-mport AppController from '../controllers/AppController';
+import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
 /**
- *  * It binds routes to appropriate handler in
- *   * given Express application.
- *    * @param {Express} app The Express application.
- *     */
+ * It binds routes to appropriate handler in
+ * given Express application.
+ * @param {Express} app The Express application.
+ */
 const mapRoutes = (app) => {
-	  app.get('/', AppController.getHomepage);
-	  app.get('/students', StudentsController.getAllStudents);
-	  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
 };
 
 export default mapRoutes;
