b5699997795fd857c5e53aabab9eea53c1145795
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-04 16:40:02 +0200

    0-co

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+* Output a message to the console.
+* @param {String} message - The message to be outputted.
+* @author Joshua Chikasha https://github.com/Joshua1821
+*/
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
