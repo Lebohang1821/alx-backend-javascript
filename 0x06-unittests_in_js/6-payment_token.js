8a26bed95d7d1b6d7f258d4fb79f0a08b30b44c4
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-05 16:25:17 +0200

    6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,4 +1,5 @@
 const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+    // If success is true, resolve promise with successful response
   if (success) {
     resolve({data: 'Successful response from the API'});
   }
