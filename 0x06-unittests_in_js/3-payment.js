76d494f3870e1c4c5378403db39cee5d74b4f703
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-05 15:52:22 +0200

    3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,5 +1,6 @@
 const Utils = require('./utils');
 
+// Function to send payment request to API
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${totalCost}`);
